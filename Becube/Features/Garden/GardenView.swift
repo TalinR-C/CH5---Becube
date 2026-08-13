8cb1740c74354421963d8881161d591e740c4379
David Paul Ong <davidpaulong@gmail.com>
2026-08-13 15:10:06 +0800

    fix JSON decode

--- Becube/Features/Garden/GardenView.swift
+++ Becube/Features/Garden/GardenView.swift
@@ -5,6 +5,15 @@
 //  Created by Talin Russo-Christoffelsz on 13/08/26.
 //
 
-import Foundation
+import SwiftUI
 
-// TODO: implement
+struct GardenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(ContentRepository.skills[0].name)
+    }
+}
+
+#Preview {
+    GardenView()
+}
