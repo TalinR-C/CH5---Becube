6547fe565974a7ce8d80481a625b74becdb03c45
TalinR-C <talinphoenix@gmail.com>
2026-08-13 14:07:49 +0800

    first head

---
+++ Becube/BecubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BecubeApp.swift
+//  Becube
+//
+//  Created by Talin Russo-Christoffelsz on 13/08/26.
+//
+
+import SwiftUI
+
+@main
+struct BecubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
